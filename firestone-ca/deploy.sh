135b918c6b7f5de5ba646f2a8abde19304f7c6cb
xuews <xuews@hsmap.com>
2023-02-20 17:15:09 +0800

    基于docker network构建网络

--- firestone-ca/deploy.sh
+++ firestone-ca/deploy.sh
@@ -2,13 +2,14 @@
 path=`pwd`
 
 docker build -t chainmaker-ca:v2.2.0 .
-
+docker network create ca-network 
 echo "start mysql service..."
 docker run -d \
+    -p 13306:3306 \
     -e MYSQL_ROOT_PASSWORD=123456 \
     -e MYSQL_DATABASE=chainmaker_ca \
+    --network=ca-network \
     --name ca-mysql \
-    --net host \
     --restart always \
     mysql:8.0
 echo "waiting for database initialization..."
@@ -17,14 +18,15 @@ docker logs --tail=10 ca-mysql
 
 echo "start ca services..."
 docker run -d \
+-p 8096:8090 \
 -w /chainmaker-ca \
 -v $path/src/conf:/chainmaker-ca/conf \
 -v $path/log:/log \
 -v $path/crypto-config:/crypto-config \
 -v $path/sansec-pkcs11/pkcs11:/usr/local/lib64/pkcs11 \
 -v $path/sansec-pkcs11/etc:/etc \
---net host \
 --name ca-server \
+--network=ca-network  \
 --restart always \
 chainmaker-ca:v2.2.0 \
 bash -c "./chainmaker-ca -config ./conf/config.yaml"
